9dbf981e5b65877394a060526dbe51138fadd32b
sanchezzzhak <sanchezzzhak@ya.ru>
2023-10-13 20:08:15 +0300

    remove datamanager added reactive to column and data added sort columns

--- src/components/GridView.vue
+++ src/components/GridView.vue
@@ -1,7 +1,6 @@
 <script setup>
 
-import {defineProps, toRefs} from 'vue';
-import DataManager from './grid-view/data-manager';
+import {defineProps, reactive, computed} from 'vue';
 
 const props = defineProps([
   'id',
@@ -9,76 +8,183 @@ const props = defineProps([
   'tableResponsiveMobile',
   'theadSticky',
   'resizeColumn',
-
   'containerOptions',
   'tableOptions',
   'theadOptions',
   'columns',
   'data',
+  'page',
+  'pageSize',
+  'pageTotal',
 ]);
-
-// const theme = props.config?.theme ?? 'bootstrap5';
 
 const getClasses = (list) => {
   let result = {};
   list.forEach((value) => {
     result[value] = true;
-  })
+  });
   return result;
+};
+
+function columnKey(column) {
+  return column.key;
 }
+
+function columnType(column) {
+  return column.type;
+}
+
+function columnLabel(column) {
+  return column.label;
+}
+
+function columnCell(column, row) {
+  return row[column.key] ?? '';
+}
+
+function columnSortType(column) {
+  return column.sortType ?? '';
+}
+
+const table = reactive({
+  data: props.data,
+  columns: props.columns
+})
+
+const setting = reactive({
+  order: null,
+  pagination: {
+    page: props.page,
+    size: props.pageSize,
+    max: computed(() => {
+      if (props.pageTotal <= 0) {
+        return 0;
+      }
+      let maxPage = Math.floor(props.pageTotal / setting.pagination.size);
+      if (props.pageTotal % setting.pagination.size > 0) {
+        maxPage++;
+      }
+      return maxPage;
+    }),
+    offset: computed(() => {
+      return (setting.pagination.page - 1) * setting.pagination.size + 1;
+    }),
+    limit: computed(() => {
+      let limit = setting.pagination.page * setting.pagination.size;
+      return props.pageTotal >= limit ? limit : props.pageTotal;
+    }),
+  },
+});
+
+const rows = computed(() => {
+  let rows = table.data;
+  let columns = table.columns;
+
+  const sorts = [];
+
+  const sortLexically = (p, d = 1) => (a, b) => d * a[p].localeCompare(b[p]);
+  const sortNumerically = (p, d = 1) => (a, b) => d * (a[p] - b[p]);
+  const sortBy = sorts => (a, b) => sorts.reduce((r, s) => r || s(a, b), 0);
+
+  columns.forEach((column) => {
+    let key = columnKey(column);
+    let type = columnType(column);
+    let sort = columnSortType(column);
+
+    if (sort !== 'none' && sort !== '') {
+      if (sort === 'desc') {
+        sorts.push(
+            type !== 'numeric'
+                ? sortLexically(key, -1)
+                : sortNumerically(key, -1),
+        );
+      } else if (sort === 'asc') {
+        sorts.push(
+            type !== 'numeric'
+                ? sortLexically(key)
+                : sortNumerically(key),
+        );
+      }
+    }
+  });
+
+  return sorts.length > 0
+      ? rows.sort(sortBy(sorts))
+      : rows;
+});
+
+
 
 const containerClasses = {
   'table-responsive': props.tableResponsive ?? false,
   'table-responsive-mobile': props.tableResponsiveMobile ?? false,
-  ...getClasses(props.containerOptions?.class ?? [])
+  ...getClasses(props.containerOptions?.class ?? []),
 };
 
 const tableClasses = {
   'table': true,
-  ...getClasses(props.tableOptions?.class ?? [])
+  ...getClasses(props.tableOptions?.class ?? []),
 };
 
 const theadClasses = {
   'thead-sticky': props.theadSticky ?? false,
-  ...getClasses(props.theadOptions?.class ?? [])
+  ...getClasses(props.theadOptions?.class ?? []),
 };
 
-const {data, columns} = toRefs(props);
-const dataManager = new DataManager(columns, data);
+function onClickSort(column){
+  if (!column.sortable) {
+    return;
+  }
+
+  const sorts = {
+    'none' : 'asc',
+    'asc': 'desc',
+    'desc': 'none'
+  };
+  column.sortType = sorts[column.sortType] ?? 'none';
+
+
+}
 
 
 </script>
 
 <template>
   <div :class="containerClasses">
+    <div v-if="isLoading" class="gridview-loading-mask">
+      <div class="vtl-loading-content">
+        <span style="color: white">Loading...</span>
+      </div>
+    </div>
     <table :class="tableClasses">
       <thead :class="theadClasses">
       <tr>
         <th
-            v-for="column of dataManager.columns()"
+            v-for="(column,index) of table.columns"
             v-bind:key="column"
-            :data-key="column.key()"
-            :data-index="column.index()"
-            :data-type="column.type()"
+            :data-key="columnKey(column)"
+            :data-index="index"
+            :data-type="columnType(column)"
             scope="col"
             :class="[{
                 sortable: column.sortable,
-                'none': column.sortable && column.sortType === 'none',
-                'desc': column.sortable && column.sortType === 'desc',
-                'asc': column.sortable && column.sortType === 'asc',
             }]"
-        >{{ column.label() }} <span v-if="resizeColumn ?? false" class="resize-handle"></span>
+            :data-sort-dir="columnSortType(column)"
+        >
+          <a @click.prevent="onClickSort(column)">{{ columnLabel(column) }}</a>
+          <span v-if="resizeColumn ?? false" class="resize-handle"></span>
         </th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="row of dataManager.data()"  v-bind:key="row">
-        <td v-for="column of dataManager.columns()"
+      <tr v-for="row of rows" v-bind:key="row">
+        <td v-for="(column,index) of table.columns"
             v-bind:key="column"
-            :data-key="column.key()"
-            :data-label="column.label()"
-            :scope ="column.index() === 0 ? 'row': void 0"
-        >{{ column.render(row) }}</td>
+            :data-key="columnKey(column)"
+            :data-label="columnLabel(column)"
+            :scope="index === 0 ? 'row': void 0"
+        >{{ columnCell(column, row) }}
+        </td>
       </tr>
       </tbody>
     </table>
@@ -86,14 +192,25 @@ const dataManager = new DataManager(columns, data);
 </template>
 
 <style>
+th[data-sort-dir="desc"].sortable:after {
+  content: "\25bc";
+  font-size: 0.7em;
+  padding-left: 3px;
+  line-height: 0.7em;
+
+}
+th[data-sort-dir="asc"].sortable:after {
+  content: "\25b2";
+  font-size: 0.7em;
+  padding-left: 3px;
+  line-height: 0.7em;
+}
 
 .thead-sticky th {
   position: sticky;
   top: 0;
-  background: #6c7ae0;
   font-weight: normal;
   font-size: 1.1rem;
-  color: white;
 }
 
 @media screen and (max-width: 600px) {
@@ -116,7 +233,7 @@ const dataManager = new DataManager(columns, data);
     width: 1px;
   }
 
-  .table-responsive-mobile  table tr {
+  .table-responsive-mobile table tr {
     border-bottom: 3px solid #ddd;
     display: block;
   }
